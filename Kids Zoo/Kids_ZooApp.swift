8858cd36b6a13909e4a32eaf0eb56adf705051d4
RamezHamdiSaeed <ramez.hamdi4work@gmail.com>
2024-04-05 14:42:13 -0700

    Initial Commit

---
+++ Kids Zoo/Kids_ZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_ZooApp.swift
+//  Kids Zoo
+//
+//  Created by Ramez Hamdi Saeed on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kids_ZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
